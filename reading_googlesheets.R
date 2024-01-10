5aaf6d602cbc52bd01be5d2b9ba4d1d266339ebb
bjbutterfield <bjbutterfield@gmail.com>
2024-01-10 09:30:24 -0700

    Initiating code

---
+++ reading_googlesheets.R
@@ -0,0 +1,7 @@
+# Code for reading google sheets into R
+# dependencies - 'googlesheets4'
+# the first time running this code, be sure to run the following line(s) to install the necessary dependency(ies) after removing the '#'
+#install.packages('googlesheets4')
+
+#load the required library
+library(googlesheets4)
